f16df3f8f5a31e37e44405d624d9e076a8386001
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-19 12:26:10 +0300

    Creates user class User

---
+++ user.py
@@ -0,0 +1,12 @@
+class User : 
+    user_list = [] 
+    def __init__(self, first_name,last_name,email,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def save_user(self): 
+        User.user_list.append(self)
+
+    
