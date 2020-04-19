f9ad006b245c55c1fa403138f8f878b4bdd2bca6
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-19 14:36:28 +0300

    Adds delete_account function

--- credentials_test.py
+++ credentials_test.py
@@ -5,12 +5,22 @@ class TestCredentials (unittest.TestCase):
 
     def setUp(self):
         self.new_account = Credentials ("Twitter", "Bre", "bw@38479")
-    
+
     def tearDown(self):
         Credentials.account_info = []
+    
+    def test_init(self):
+        self.assertEqual(self.new_account.account, "Twitter")
+        self.assertEqual(self.new_account.username, "Bre")
+        self.assertEqual(self.new_account.password, "bw@38479")
 
     def test_new_account(self):
         self.new_account.save_account()
+
+    def test_delete_account(self):
+        self.new_account.save_account()
+        self.new_account.delete_account()
+        self.assertEqual(len(Credentials.account_info), 0)
 
 if __name__ == '__main__':
     unittest.main()
